f2a74f069fbc3d814d61e1add8f292b5fb46cd05
unknown <sarikjain1234@gmail.com>
2025-09-10 20:27:56 +0530

    strings and slicing

---
+++ strings.py
@@ -0,0 +1,16 @@
+name="sarik"
+#this is a string
+
+
+#slicing
+
+shortname=name[3]
+print(shortname)
+shortname1=name[0:3]
+print(shortname1)
+shortname2=name[:4]
+print(shortname2)
+
+
+
+print(name[3:])
