81e6b419141d93de6c1bb204e9a408dff9790690
unknown <sarikjain1234@gmail.com>
2025-09-10 12:46:21 +0530

    input function

---
+++ input.py
@@ -0,0 +1,6 @@
+a=input("enter number 1 : ")
+b=input("enter number 2 : ")            #type is string
+print(a+b)
+a=int(input("enter number 1 : "))
+b=int(input("enter number 2 : "))       #type is integer
+print(a+b)
