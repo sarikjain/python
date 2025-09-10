1d9ee5a3f1ca05434bb373d84ed07cbc830ab3f5
unknown <sarikjain1234@gmail.com>
2025-09-10 20:52:37 +0530

    escape sequence

---
+++ escape_sequence.py
@@ -0,0 +1,7 @@
+#new line
+intro="sarik is good \n and he is innocent"
+intro1="sarik is op\tand he is super"
+intro2="i am backslash \\ and i am double quote \""
+print(intro)
+print(intro1)
+print(intro2)
