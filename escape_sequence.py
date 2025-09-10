866ad8d5db6c8c7ea9822e2e2d1539d4a712d16c
unknown <sarikjain1234@gmail.com>
2025-09-10 21:26:40 +0530

    modifications

--- escape_sequence.py
+++ escape_sequence.py
@@ -5,3 +5,9 @@ intro2="i am backslash \\ and i am double quote \""
 print(intro)
 print(intro1)
 print(intro2)
+
+
+#escape sequence helps to use some commands under restriction
+
+name="sarik"
+print("good afternoon",name)
--- strings_functions.py
+++ strings_functions.py
@@ -2,5 +2,13 @@ name="sarik"
 integer=len(name)
 name1=name.endswith("l")
 print(name1)   #answer is false so return value is boolean
-name2=name.startswith(s)
-print(name2)   
+name2=name.startswith("s")
+print(name2)  
+print(name.capitalize()) 
+print(name.upper()) 
+print(name.lower())
+print(name.replace("sarik","nakul"))
+print(name.find("s"))
+
+
+#major string functions
