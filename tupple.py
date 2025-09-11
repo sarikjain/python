2f6afd4f37c36b0c1589d47d1466a3b9502d9e90
unknown <sarikjain1234@gmail.com>
2025-09-11 11:20:44 +0530

    tupple

---
+++ tupple.py
@@ -0,0 +1,27 @@
+#tupple is kind of list but immutable
+a=(1,2,3,"sarik",False)
+print(a)
+print(type(a))
+
+
+print(a[0])
+
+#singleton tupple
+b=(1,)    #single element
+c=(1)      #int datatype
+no=a.count(1)
+print(no)
+no1=a.index(3)
+print(no1)
+no2=a*2
+print(no2)
+
+i=(8,9)
+no4=a+i
+print(no4)
+
+
+print(len(i))
+print(len(a))
+
+print(sum(i))
