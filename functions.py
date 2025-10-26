3bade69092d555327d0178e472857bffba5dd92d
unknown <sarikjain1234@gmail.com>
2025-10-26 16:57:18 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,20 @@
+name="sarik"
+print("hello python")
+
+def add(a,b):
+    sum=a+b
+    return sum
+
+list1=[1,2,"sarik"]
+
+def listp(likop):
+  for i in range(0,3):
+    print(likop[i])
+  
+listp(list1)
+
+
+
+for number in list1:
+    print(number)
+  
