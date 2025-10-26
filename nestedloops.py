eee9a4f6aa746352af342d8e2340b28ae4fa4999
unknown <sarikjain1234@gmail.com>
2025-10-26 17:16:17 +0530

    nested loops

---
+++ nestedloops.py
@@ -0,0 +1,9 @@
+students=["sarik","nakul"]
+marks=[[30,40,50],[45,67,89]]
+for i in range(len(students)):
+ for j in range(len(marks[i])):
+  print("marks of ",students[i],"in subject",j+1,"are ",marks[i][j])
+
+
+
+  
